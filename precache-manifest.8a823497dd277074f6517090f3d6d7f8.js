8762a2882ac3fb842482ec26d16f08d3a6ab568b
Kojiro Ozaki <koozaki@yahoo-corp.jp>
2019-03-22 00:13:59 +0900

    Updates

---
+++ precache-manifest.8a823497dd277074f6517090f3d6d7f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c78cdab9911b81a52b84",
+    "url": "/static/js/main.c78cdab9.chunk.js"
+  },
+  {
+    "revision": "eec3fb0c7e9d09046e62",
+    "url": "/static/js/2.eec3fb0c.chunk.js"
+  },
+  {
+    "revision": "c78cdab9911b81a52b84",
+    "url": "/static/css/main.37c6d2b3.chunk.css"
+  },
+  {
+    "revision": "641016a5b7aaf3cc65dc1dcecc13f026",
+    "url": "/index.html"
+  }
+];
